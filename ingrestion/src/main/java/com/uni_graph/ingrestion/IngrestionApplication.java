31139576fb0ee12219ad33fd05de4fbf50a9df7f
Phongdnh-uit <dangnguyenhuyphong@gmail.com>
2026-03-27 14:56:41 +0700

    🧹 chore(server): init ingrestion service

---
+++ ingrestion/src/main/java/com/uni_graph/ingrestion/IngrestionApplication.java
@@ -0,0 +1,13 @@
+package com.uni_graph.ingrestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngrestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngrestionApplication.class, args);
+	}
+
+}
---
+++ ingrestion/src/test/java/com/uni_graph/ingrestion/IngrestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uni_graph.ingrestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngrestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
