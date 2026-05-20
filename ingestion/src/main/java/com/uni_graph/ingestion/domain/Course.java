7a06a9c8949a28da9ba335a3fdbcccdf332ca8a6
Phongdnh-uit <dangnguyenhuyphong@gmail.com>
2026-05-20 09:38:08 +0700

    domain: add knowledge_prerequisite and fix @Data stackoverflow risk

--- ingestion/src/main/java/com/uni_graph/ingestion/domain/Course.java
+++ ingestion/src/main/java/com/uni_graph/ingestion/domain/Course.java
@@ -2,15 +2,19 @@ package com.uni_graph.ingestion.domain;
 
 import java.util.ArrayList;
 import java.util.List;
-import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.neo4j.core.schema.Id;
 import org.springframework.data.neo4j.core.schema.Node;
 import org.springframework.data.neo4j.core.schema.Relationship;
 
 @Node("Course")
-@Data
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Course {
-  @Id private String code;
+  @Id @EqualsAndHashCode.Include private String code;
 
   private String titleVn;
   private String titleEn;
