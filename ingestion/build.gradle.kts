2824e0a5126a414c54274429eee5d68e0edc13fb
Phongdnh-uit <dangnguyenhuyphong@gmail.com>
2026-05-20 15:16:45 +0700

    refactor: update ingestion module to use common module

--- ingestion/build.gradle.kts
+++ ingestion/build.gradle.kts
@@ -1,68 +1,19 @@
 plugins {
-	java
-    id("com.diffplug.spotless") version "8.2.1"
-	id("org.springframework.boot") version "4.0.4"
-	id("io.spring.dependency-management") version "1.1.7"
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
 }
-
-group = "com.uni-graph"
-version = "0.0.1-SNAPSHOT"
-
-spotless {
-    java {
-        target("**/src/**/*.java")
-        googleJavaFormat()
-        removeUnusedImports()
-        trimTrailingWhitespace()
-        endWithNewline()
-    }
-}
-
-java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(25)
-	}
-}
-
-configurations {
-	compileOnly {
-		extendsFrom(configurations.annotationProcessor.get())
-	}
-}
-
-repositories {
-	mavenCentral()
-}
-
-extra["springAiVersion"] = "2.0.0-M3"
 
 dependencies {
-    var neo4jMigrationsVersion = "3.3.0"
-
-	implementation("org.springframework.boot:spring-boot-starter-data-neo4j")
-	implementation("org.springframework.boot:spring-boot-starter-webmvc")
-    implementation ("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.1")
+    implementation(project(":common"))
+    implementation("org.springframework.boot:spring-boot-starter-data-neo4j")
+    implementation("org.springframework.boot:spring-boot-starter-webmvc")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.1")
     implementation("com.opencsv:opencsv:5.9")
-    implementation ("eu.michael-simons.neo4j:neo4j-migrations-spring-boot-starter:$neo4jMigrationsVersion")
+    implementation("eu.michael-simons.neo4j:neo4j-migrations-spring-boot-starter:3.3.0")
     implementation("dev.langchain4j:langchain4j:0.36.2")
     implementation("dev.langchain4j:langchain4j-ollama:0.36.2")
-	compileOnly("org.projectlombok:lombok")
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
-	// developmentOnly("org.springframework.ai:spring-ai-spring-boot-docker-compose")
-	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-	annotationProcessor("org.projectlombok:lombok")
-	testImplementation("org.springframework.boot:spring-boot-starter-data-neo4j-test")
-	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-}
-
-dependencyManagement {
-	imports {
-		mavenBom("org.springframework.ai:spring-ai-bom:${property("springAiVersion")}")
-	}
-}
-
-tasks.withType<Test> {
-	useJUnitPlatform()
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-data-neo4j-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
 }
