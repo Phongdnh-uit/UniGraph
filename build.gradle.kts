1e0d84a36961675ca3b6d152f09e58dcbd7f04df
Phongdnh-uit <dangnguyenhuyphong@gmail.com>
2026-05-21 09:14:12 +0700

    🔨 refactor(server): refactor to multi-module

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,34 @@
 plugins {
     java
-    id("com.diffplug.spotless") version "8.2.1" apply false
+    id("com.diffplug.spotless") version "8.2.1"
     id("org.springframework.boot") version "4.0.6" apply false
     id("io.spring.dependency-management") version "1.1.7" apply false
+}
+
+spotless {
+    java {
+        target("**/src/**/*.java")
+        googleJavaFormat()
+        removeUnusedImports()
+        trimTrailingWhitespace()
+        endWithNewline()
+    }
+}
+
+allprojects {
+    group = "com.uit.unigraph"
+    version = "0.0.1-SNAPSHOT"
+    description = "A unified graph database management system for heterogeneous graph databases."
+    repositories {
+        mavenCentral()
+    }
 }
 
 subprojects {
     apply(plugin = "java")
     apply(plugin = "com.diffplug.spotless")
 
-    group = "com.uni-graph"
+    group = "com.unigraph"
     version = "0.0.1-SNAPSHOT"
 
     repositories {
@@ -18,21 +37,17 @@ subprojects {
 
     java {
         toolchain {
-            languageVersion = JavaLanguageVersion.of(21)
-        }
-    }
-
-    extensions.configure<com.diffplug.gradle.spotless.SpotlessExtension> {
-        java {
-            target("**/src/**/*.java")
-            googleJavaFormat()
-            removeUnusedImports()
-            trimTrailingWhitespace()
-            endWithNewline()
+            languageVersion = JavaLanguageVersion.of(25)
         }
     }
 
     tasks.withType<Test> {
         useJUnitPlatform()
+    }
+
+    configurations {
+        compileOnly {
+            extendsFrom(configurations.annotationProcessor.get())
+        }
     }
 }
