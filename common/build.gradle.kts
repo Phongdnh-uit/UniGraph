0adceaf2661fba6f92743a57c3c1a35522a4a349
Phongdnh-uit <dangnguyenhuyphong@gmail.com>
2026-05-25 14:21:41 +0700

    🐛 fix(server): fix mismatch common dependencies

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -5,6 +5,6 @@ plugins {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-neo4j:4.0.6")
     implementation("org.springframework.boot:spring-boot-starter-webmvc:4.0.6")
-    compileOnly("org.projectlombok:lombok:1.18.36")
-    annotationProcessor("org.projectlombok:lombok:1.18.36")
+    compileOnly("org.projectlombok:lombok:1.18.46")
+    annotationProcessor("org.projectlombok:lombok:1.18.46")
 }
